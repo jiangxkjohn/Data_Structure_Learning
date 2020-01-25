ca8dc9ddb73d4467948428c9e6cfc79f6fd4972e
John Jiang <781768633@qq.com>
2020-01-25 14:30:26 +0800

    add linear list

---
+++ linear_list/include/list.h
@@ -0,0 +1,10 @@
+#define MAXSIZE 1000
+typedef int ElementType;
+typedef int Position;
+typedef struct LNode * PtrToLNode;
+struct LNode{
+    /* data */
+    ElementType Data[MAXSIZE];
+    Position Last;
+};
+typedef PtrToLNode List;
